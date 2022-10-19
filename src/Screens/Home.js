becedd50706b31cf94f367e0f1fbe75c203be3dc
99004484-Rounak <rounakstanley@gmail.com>
2022-10-20 00:40:57 +0530

    Add image and title

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import './Home.css'
+import Monitor from '../Assets/Monitor.png'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className = "introScreen">
+        <div className = "mainFlexContainer">
+          <div className = "introName">
+              Rounak Stanley
+          </div>
+          <div className = "pictureContainer">
+            <img className = "monitorImage" src = {Monitor} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
